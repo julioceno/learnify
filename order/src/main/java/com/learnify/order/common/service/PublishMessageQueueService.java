d54035d82821192547a38bf4bc0ca9b83ff28470
Júlio Nepomuceno <julioreplay@gmail.com>
2024-11-06 21:50:42 -0300

    feat: create config for public message in sqs queue

--- order/src/main/java/com/learnify/order/common/service/PublishMessageQueueService.java
+++ order/src/main/java/com/learnify/order/common/service/PublishMessageQueueService.java
@@ -1,19 +1,44 @@
 package com.learnify.order.common.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.learnify.order.common.exception.BadRequestException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
+import com.amazonaws.services.sqs.AmazonSQS;
 
 @Service
-public class PublicMessageQueueService {
+@Slf4j
+public class PublishMessageQueueService {
     @Value("${queueUrl}")
     private String queueUrl;
 
-    public void run() {
-        SendMessageRequest send_msg_request = new SendMessageRequest()
-                .withQueueUrl(queueUrl)
-                .withMessageBody("hello world")
-                .withDelaySeconds(5);
-        sqs.sendMessage(send_msg_request);
+    @Autowired
+    private AmazonSQS amazonSQS;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    public void run(final String queueUrl, Object body) {
+        log.info("Create object to send message");
+        String converted = convertObjectToJson(body);
+        var message = new com.amazonaws.services.sqs.model.SendMessageRequest()
+               .withQueueUrl(queueUrl)
+               .withMessageBody(converted);
+
+        log.info("Object created, sending message to queue...");
+        amazonSQS.sendMessage(message);
+        log.info("Messaged sent");
+    }
+
+    private String convertObjectToJson(Object body) {
+        try {
+            return objectMapper.writeValueAsString(body);
+        } catch (JsonProcessingException e) {
+            log.error("Ocurred error when try convert object to string", e);
+            throw new BadRequestException("Ocorreu um erro ao prosseguir a operação, entre em contato com a equipe de suporte");
+        }
     }
 }
--- order/src/main/java/com/learnify/order/infra/configuration/SqsConfiguration.java
+++ order/src/main/java/com/learnify/order/infra/configuration/SqsConfiguration.java
@@ -1,4 +1,16 @@
 package com.learnify.order.infra.configuration;
 
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
 public class SqsConfiguration {
+
+    @Bean
+    public AmazonSQS amazonAsyncHttpClient() {
+        return AmazonSQSClientBuilder.defaultClient();
+   }
 }
